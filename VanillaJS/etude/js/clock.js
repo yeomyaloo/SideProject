32a6e3a5a7d877160262257b639b8fd4930b7ce9
hwiju yeom <yeom456@gmail.com>
2021-12-31 23:57:40 +0900

    12/31
    올해의 마지막 CSS

--- VanillaJS/etude/js/clock.js
+++ VanillaJS/etude/js/clock.js
@@ -1,11 +1,11 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("div#clock");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  clock.innerText = `현재시각 : ${hours}:${minutes}`;
 }
 
 getClock();
--- VanillaJS/etude/js/todo.js
+++ VanillaJS/etude/js/todo.js
@@ -24,7 +24,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "X";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
