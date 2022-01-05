2f1c33957d4e1d0ed6082f3968560a8366eac87b
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2022-01-05 17:30:43 +0900

    1/5
    javascript를 이용해서 계산기 만들기

--- VanillaJS/etude/js/background.js
+++ VanillaJS/etude/js/background.js
@@ -1,6 +1,6 @@
 //Array를 랜덤하게 골라내는 작업
 
-const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
+const images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
 "6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"];
 
 // floor은 버림 math 함수임.  images 안에 변수만큼 랜덤하게 골라주는 것
