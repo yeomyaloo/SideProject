91ff5b6b2321ee19c206bdd3e3a039b36c98e362
hwiju yeom <yeom456@gmail.com>
2022-01-06 22:46:56 +0900

    1/6

--- MBTI/Panda_MBTI/js/start.js
+++ MBTI/Panda_MBTI/js/start.js
@@ -1,20 +1,43 @@
-const main = document.querySelector('#main');
-const qna = document.querySelector('#qna');
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function addAnswer(answerText,qIdx){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('Button');
+    answer.classList.add('answerList');
+    a.appendChild(answer); // answer라는 버튼이 a에 소속될 수 있게 
+    answer.innerHTML = answerText;
+    answer.addEventListener("click", function(){
+
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0 ; i < children.length; i++){
+            children[i].disabled = true;
+            children[i].style.display='none'; 
+        }
+        goNext(++qIdx);
+    }, false);
+}
+function goNext(qIdx){
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer,qIdx);   
+    }
+}
+
 
 
 function begin(){
-    /*main.style.display = "none";
-    qna.style.display = "block";*/
-    main.style.webkitAnimation = "fadeOut 1s";
-    main.style.animation = "fadeOut 1s";
-    
+  main.style.WebkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
-        
-        qna.style.webkitAnimation = "fadeIn 1s";
-        qna.style.animation = "fadeIn 1s";
-        setTimeout(() => {
-            main.style.display="none";
-            qna.style.display = "block";
-        }, 450)
-    }, 450);
+      main.style.display = "none";
+      qna.style.display = "block"
+    }, 450)
+    let qIdx =0;
+    goNext(qIdx);
+  }, 450);
 }
