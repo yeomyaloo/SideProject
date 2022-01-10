838e1009faa38b266d0e143881b8309ceafc7db8
hwiju yeom <yeom456@gmail.com>
2022-01-10 21:25:15 +0900

    1/10

--- MBTI/Panda_MBTI/js/start.js
+++ MBTI/Panda_MBTI/js/start.js
@@ -1,31 +1,59 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
-function addAnswer(answerText,qIdx){
-    var a = document.querySelector('.answerBox');
-    var answer = document.createElement('Button');
-    answer.classList.add('answerList');
-    a.appendChild(answer); // answer라는 버튼이 a에 소속될 수 있게 
-    answer.innerHTML = answerText;
-    answer.addEventListener("click", function(){
+const endPoint = 12;
 
-        var children = document.querySelectorAll('.answerList');
-        for(let i = 0 ; i < children.length; i++){
-            children[i].disabled = true;
-            children[i].style.display='none'; 
-        }
-        goNext(++qIdx);
-    }, false);
+
+function goResult(){
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";
+      result.style.display = "block"
+    }, 450)})
+    setResult();
 }
-function goNext(qIdx){
-    var q = document.querySelector('.qBox');
-    q.innerHTML = qnaList[qIdx].q;
-    for(let i in qnaList[qIdx].a){
-        addAnswer(qnaList[qIdx].a[i].answer,qIdx);   
+
+function addAnswer(answerText, qIdx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
+
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
     }
+    setTimeout(() => {
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },450)
+  }, false);
 }
 
-
+function goNext(qIdx){
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+  var status = document.querySelector('.statusBar');
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
+}
 
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
@@ -37,7 +65,7 @@ function begin(){
       main.style.display = "none";
       qna.style.display = "block"
     }, 450)
-    let qIdx =0;
+    let qIdx = 0;
     goNext(qIdx);
   }, 450);
 }
