4c902d1b18ef5571f5fbdb8e18329819aea9a2a2
hwiju yeom <yeom456@gmail.com>
2022-01-12 22:28:29 +0900

    1/12

--- MBTI/Panda_MBTI/js/start.js
+++ MBTI/Panda_MBTI/js/start.js
@@ -1,7 +1,48 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
-
+const result = document.querySelector("#result")
 const endPoint = 12;
+const select = [];
+
+function calResult(){
+  var pointArray = [
+    {name:'mouse', value: 0, key: 0},
+    {name:'cow', value: 0, key: 1},
+    {name:'tiger', value: 0, key: 2},
+    {name:'rabbit', value: 0, key: 3},
+    {name:'dragon', value: 0, key: 4},
+    {name:'snake', value: 0, key: 5},
+    {name:'horse', value: 0, key: 6},
+    {name:'sheep', value: 0, key: 7},
+    {name:'monkey', value: 0, key: 8},
+    {name:'chick', value: 0, key: 9},
+    {name:'dog', value: 0, key: 10},
+    {name:'pig', value: 0, key: 11}
+  ]
+
+  for (let i = 0; i < endPoint; i++ ){
+    var target = qnaList[i].a[select[i]];
+    for (let j = 0; j < target.length; j++){
+      for(let k = 0; k < pointArray.length; k++){
+        if(target.type[j]===pointArray[k].name){
+          pointArray[k].value += 1;
+        }
+      }
+    }
+  }
+  var resultArray = pointArray.sort(function(a,b){
+    if(a.value > b.value){
+      return -1
+    }
+    if(a.value < b.value){
+      return 1;
+    }
+    return 0;
+  });
+
+  let resultword = resultArray[0].key;
+  return resultword;
+}
 
 
 function goResult(){
@@ -37,6 +78,7 @@ function addAnswer(answerText, qIdx){
       children[i].style.animation = "fadeOut 0.5s";
     }
     setTimeout(() => {
+      select[qIdx] = idx;
       for(let i = 0; i < children.length; i++){
         children[i].style.display = 'none';
       }
@@ -46,6 +88,11 @@ function addAnswer(answerText, qIdx){
 }
 
 function goNext(qIdx){
+
+  if (qIdx+1 ===  endPoint){
+    goResult();
+    return;
+  }
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
